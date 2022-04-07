27eb5746977bca8bdcab49fec74b7021bffdf72d
55Nights <1905587@students.kca.ac.ke>
2022-04-07 11:16:58 -0700

    printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-       printf("size of char : %lu\n",sizeof(char));
+       printf("size of char : %lu\n", sizeof(char));
 
-       printf("size of int : %d\n",sizeof(int));
+       printf("size of int : %d\n", sizeof(int));
+   
+       printf("size of long int : %lu\n", sizeof(long int));
 
-       printf("size of long int : %lu\n",sizeof(long int));
+       printf("size of long long int : %lu\n", sizeof(long long int));
 
-       printf("size of long long int : %lu\n",sizeof(long long int));
-
-       printf("size of float : %d\n",sizeof(float));
+       printf("size of float : %d\n", sizeof(float));
 
        return (0);
 }
