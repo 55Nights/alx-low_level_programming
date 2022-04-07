c0af67501651fe7b540c190626c23cb797886799
55Nights <1905587@students.kca.ac.ke>
2022-04-07 11:12:55 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+       printf("size of char : %lu\n",sizeof(char));
+
+       printf("size of int : %d\n",sizeof(int));
+
+       printf("size of long int : %lu\n",sizeof(long int));
+
+       printf("size of long long int : %lu\n",sizeof(long long int));
+
+       printf("size of float : %d\n",sizeof(float));
+
+       return (0);
+}
