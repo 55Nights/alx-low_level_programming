f0a895d3c2b00875471201ed6efd7c9dc89c0ad5
55Nights <1905587@students.kca.ac.ke>
2022-04-07 10:01:52 -0700

    4-put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 
+/*
+ * this is a program to print str
+ */
+
 int main(void)
 {
 	char str[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 
-	return 0;
+	return (0);
 }
 
