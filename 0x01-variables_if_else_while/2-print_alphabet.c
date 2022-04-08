b97a65479a28d0b4ceee381b96abaa61e4659276
55Nights <1905587@students.kca.ac.ke>
2022-04-08 01:19:43 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: (0) if succesfull
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		
+		c++;
+	}
+
+	return (0);
+}
