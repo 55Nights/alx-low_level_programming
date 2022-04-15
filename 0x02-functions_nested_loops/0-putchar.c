7d4b41bc22ebce6531a7fb0b2965f603d23d8ad6
55Nights <1905587@students.kca.ac.ke>
2022-04-15 08:24:12 +0000

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,17 @@
  * main - the entry point
  * Return: 0 if successfull
  */
-void main(void)
+int main(void)
 {
-	 char c = "_putchar\n");
+	 _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
+
+	 return (0);
 }
