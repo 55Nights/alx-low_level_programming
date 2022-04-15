f6fe0d6027370fabefcfebecf71e787968ead2b7
55Nights <1905587@students.kca.ac.ke>
2022-04-15 07:49:10 +0000

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,4 +6,20 @@
 
 int main(void)
 {
+	char a = '0';
+	char b = 'a';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
 
