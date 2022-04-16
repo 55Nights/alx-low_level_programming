773a4a24778d2d431b172017757255507c8e6735
55Nights <1905587@students.kca.ac.ke>
2022-04-16 08:48:15 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: if successfull return 1
+ * @print_alphabet prints char a
+ */
+int main(void)
+{
+	 print_alphabet()
+	{
+		char a = 'a';
+
+		for (a = 'a'; a <= 'a'; a++)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+
+}
