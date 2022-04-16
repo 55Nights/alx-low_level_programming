8bf9cde5e232e2bb3e1300d3801be6cd1071fc7f
55Nights <1905587@students.kca.ac.ke>
2022-04-16 17:09:28 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+ /**
+ * _abs - the entry point
+ * @d : -  the value to be evaluated
+ * Return: 0;
+ */
+int _abs(int d)
+{
+	int v;
+
+	v = abs(d);
+	printf("%d", v);
+
+	return (0);
+}
