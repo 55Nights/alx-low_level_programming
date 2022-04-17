7853cb3fedf13fc2a7783ab6d7a413cb6f57972c
55Nights <1905587@students.kca.ac.ke>
2022-04-17 14:41:28 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,11 +22,9 @@ void times_table(void)
 				printf("%d", product);
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
 			} else
 			{
 				printf("%d", product);
-				_putchar('$');
 			}
 		}
 		_putchar('\n');
