99bf3f4128ad4ff15e44d86fc5dc0d80951b0ee4
55Nights <1905587@students.kca.ac.ke>
2022-04-17 14:26:42 +0000

    o-putchar

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day  of Jack Bauer
+ * Return: has no return type
+ */
+void jack:wq
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * time_table - main entry point
+ * Return: -  is void
+ */
+void times_table(void)
+{
+	int product;
+	int i;
+	int j;
+	
+	i = 0;
+	j = 0;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			product = i * j;
+			if (j != 9)
+			{
+				printf("%d", product);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
