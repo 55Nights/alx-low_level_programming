13647ace26494e00750699ea94d0e1ca143b8a44
55Nights <1905587@students.kca.ac.ke>
2022-04-18 08:23:40 +0000

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,25 +5,40 @@
  */
 int main(void)
 {
-	unsigned long int c, p, num;
-	int n;
+	unsigned long int i, p, c, n, p1, p2, c1, c2;
 
-	c = 1;
-	p = 0;
-	n = 0;
-	while (n < 99)
+	p = 1;
+	c = 2;
+	i = 1;
+	n = 1000000000;
+	printf("%lu", p);
+
+	while (i < 91)
 	{
-		num = c + p;
-		if (n == 98)
-		{
-			printf("%lu", num);
-		} else
-		{
-			printf("%lu, ", num);
-		}
-		p = c;
-		c = num;
-		n++;
+		printf(", %lu", c);
+		c = c + p;
+		p = c - p;
+
+		i++;
+	}
+	p1 = (p / n);
+	p2 = (p % n);
+
+	c1 = (c1 / n);
+	c2 = (c1 % n);
+
+	i = 92;
+	while (i < 99)
+	{
+		printf(", %lu", c1 + (c2 / n));
+		printf("%lu", c % n);
+
+		c1 = c1 + p1;
+		p1 = c1 - p1;
+		c2 = c2 + p2;
+		p2 = c2 - p2;
+
+		i++;
 	}
 	putchar('\n');
 	return (0);
