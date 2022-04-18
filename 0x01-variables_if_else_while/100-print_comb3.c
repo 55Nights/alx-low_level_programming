42d8bedfa8c1b48da02497d5ade6b822e523295e
55Nights <1905587@students.kca.ac.ke>
2022-04-18 12:46:52 +0000

    100 combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i,j,k;
+	
+	k = '0';
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (j != i)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (!(i == '8' && j == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		k++;
+	}
+	putchar('\n');
+
+	return (0);
+}
