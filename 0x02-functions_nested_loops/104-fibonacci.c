6b289c3a3a4368fe94356b88925acf23edc7e81b
55Nights <1905587@students.kca.ac.ke>
2022-04-18 06:23:04 +0000

    104 -fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int c, p, num;
+	int n;
+
+	c = 1;
+	p = 0;
+	n = 0;
+	while (n < 99)
+	{
+		num = c + p;
+		if (n == 98)
+		{
+			printf("%u", num);
+		} else
+		{
+			printf("%u, ", num);
+		}
+		p = c;
+		c = num;
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
