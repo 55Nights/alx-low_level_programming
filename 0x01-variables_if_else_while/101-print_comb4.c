216002bdfe834c5b05c72b47f712563af60b4818
55Nights <1905587@students.kca.ac.ke>
2022-04-18 13:10:48 +0000

    print

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (k = j + i; k <= '9'; k++)
+			{
+				if ((j != i) != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i == '7' && k == '8')
+						continue;
+
+
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
