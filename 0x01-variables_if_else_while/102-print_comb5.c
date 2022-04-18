25d0460bcac0cdcb862c55fda48b565595e87006
55Nights <1905587@students.kca.ac.ke>
2022-04-18 13:32:22 +0000

    double

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int j,k;
+
+	for (j = 0; j <= 98; j++)
+	{
+		for (k = j + 1; k <= 99; k++)
+		{
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(' ');
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
+
+			if (j == 98 && k == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
