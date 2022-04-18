05fb9873289036ca8d0400f5cadcb2042d4f53d5
55Nights <1905587@students.kca.ac.ke>
2022-04-18 08:39:39 +0000

    time lord

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,5 +3,41 @@
  * jack_bauer - prints every minute of the day  of Jack Bauer
  * Return: has no return type
  */
-void jack:wq
+void jack_bauer(void)
+{
+	int h, h1, m, m1, s;
+
+	s = 9;
+	h1 = 0;
+	while (h1 <= 2)
+	{
+		if (h1 == 2)
+		{
+		s = 3;
+		}
+	h = 0;
+	while (h <= s)
+	{
+		m1 = 0;
+		while (m1 <= 5)
+		{
+			m = 0;
+			while (m <= 9)
+			{
+				_putchar('0' + h1);
+				_putchar('0' + h);
+				_putchar(':');
+				_putchar('0' + m1);
+				_putchar('0' + m);
+				_putchar('\n');
+
+				m++;
+			}
+			m1++;
+		}
+		h++;
+	}
+	h1++;
+	}
+}
 
