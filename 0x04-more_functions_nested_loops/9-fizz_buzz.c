cda0269e463cb90926c61322539e095d8f39c13c
55Nights <1905587@students.kca.ac.ke>
2022-04-19 10:10:32 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int main(void)
 	{
 		if (i != 100)
 		{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 
 		}
 		else if (i % 5 == 0)
@@ -21,9 +21,9 @@ int main(void)
 			printf("Buzz ");
 
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 
 		}
 		else
