5444c4dbbc1645ce7a071a5c88ede3d2d009c095
55Nights <1905587@students.kca.ac.ke>
2022-04-19 13:24:49 +0000

    100-print

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - the entry point
+ * Return: 0
+ */
+int main(void)
+{
+	long int i = 612852475143;
+	long int j;
+
+	for (j = 2; j < i; j++)
+	{
+		if (i % j == 0)
+		{
+			i = i / j;
+		}
+	}
+	printf("%ld", j);
+
+	return (0);
+}
