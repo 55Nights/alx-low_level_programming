e7445fbbfb6cfc5b36d90e59c4a072aefd1b7fc6
55Nights <1905587@students.kca.ac.ke>
2022-04-20 13:27:12 +0000

    n =98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets values to 98
+ * @n: - takes an addres of another variable
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory is about pointers and strings in c
