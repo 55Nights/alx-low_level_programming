bf0e3755093f57af2a73aa2475fededd0d36d54e
55Nights <1905587@students.kca.ac.ke>
2022-04-20 21:06:14 +0000

    copy str

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy -  copys strings
+ * @dest : first string
+ * @src : last string
+ * Return : char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (src);
+}
