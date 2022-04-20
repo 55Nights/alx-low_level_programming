a2a599bdfacaa663e5abe54c07fb566b9b3be4f5
55Nights <1905587@students.kca.ac.ke>
2022-04-20 14:43:44 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "3-puts.c"
+#include <stdio.h>
+int main(void)
+{
+	char *str;
+	str = "hello";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints str
+ * @str : the string
+ * Return : void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
