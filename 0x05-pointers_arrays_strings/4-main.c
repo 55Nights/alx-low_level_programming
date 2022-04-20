925c21fa4837be36ca8529ca6e85a72e8ce230dd
55Nights <1905587@students.kca.ac.ke>
2022-04-20 16:22:23 +0000

    reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,9 @@
+#include "_putchar.c"
+#include "4-print_rev.c"
+int main(void)
+{
+	char *str;
+	str = "hello";
+	print_rev(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_rev - reverses a string
+ * @s : string to be reversed
+ * Return : void
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	putchar('\n');
+}
