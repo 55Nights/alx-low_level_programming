5a929fe05cc5bc2ff3a7902db14557a7af602350
55Nights <1905587@students.kca.ac.ke>
2022-04-21 18:12:06 +0000

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - converts string to integer
+ * @s:  string to convert
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int r;
+
+	r = atoi(s);
+	return (r);
+}
