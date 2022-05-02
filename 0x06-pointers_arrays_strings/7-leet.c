353265e792c289c5b8d1a01d55aca105b0088f57
55Nights <1905587@students.kca.ac.ke>
2022-05-02 14:09:36 +0000

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,16 +9,17 @@ char *leet(char *s)
 {
 	int i, j;
 
-	char *_low = "aeotl";
-	char *_upper = "AEOTL";
-	char *lt = "43071";
+	i = 0;
+	char *l = "aeotl";
+	char *u = "AEOTL";
+	char *num = "43071";
 
 	while (*(s + i) != '\0')
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (*(s + i) == _low[j] || *(s + i) == _upper[j])
-				*(s + i) == _upper[j];
+			if (*(s + i) == *(u + j) || *(s + i) == *(l + j))
+				*(s + i) = *(num + j);
 		}
 		i++;
 	}
