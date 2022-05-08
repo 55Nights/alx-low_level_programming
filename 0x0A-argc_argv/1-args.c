38e8fad9e031deb60f722661a581b1d348f8263c
55Nights <1905587@students.kca.ac.ke>
2022-05-08 08:36:33 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * @argc: is the argument count
+ * @argv: is the argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0;  i < argc; i++)
+	{
+	}
+	printf("%d\n", i - 1);
+	return (0);
+}
