d71fb94b6729fac80d723fa22f998925e4940928
55Nights <1905587@students.kca.ac.ke>
2022-05-14 11:06:08 +0000

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - returns a pointer
+ * @str: the string that is to be allocated
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i = 0, size = 10;
+
+	p = malloc(size);
+	if (str == "")
+	{
+		return (NULL);
+	}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	if (strlen(str) > size)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (str[i] != '\0')
+		{
+			p[i] = str[i];
+			i++;
+		}
+	}
+	return (p);
+}
