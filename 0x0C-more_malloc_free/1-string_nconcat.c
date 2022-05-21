4c5fa5b4bb11273e9f30ec218b72c48ed99a43d7
55Nights <1905587@students.kca.ac.ke>
2022-05-21 13:35:00 +0000

    string_nc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - adds two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: integer
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int size, i, j, sz1, sz2;
+
+	if (s1 == NULL)
+	{
+		s1 = "\0";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
+	sz1 = strlen(s1);
+	sz2 = strlen(s2);
+	size = sz1 + sz2 + 1;
+	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < sz1; i++)
+	{
+		p[i] = s1[i];
+	}
+	if (n >= sz2)
+	{
+		for (j = 0; j < sz2; j++)
+		{
+			p[i + j] = s2[j];
+		}
+	}
+	else
+	{
+		for (j = 0; j < n; j++)
+		{
+			p[i + j] = s2[j];
+		}
+	}
+	p[size - 1] = '\0';
+	return (p);
+}
