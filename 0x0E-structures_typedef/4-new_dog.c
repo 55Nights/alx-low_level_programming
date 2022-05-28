5df95c5c16e590cddd56a479680b9678661605ce
55Nights <1905587@students.kca.ac.ke>
2022-05-28 13:41:25 +0000

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
 		return (NULL);
+		free(dog);
+	}
 
 	dog->name = name;
 	dog->age = age;
