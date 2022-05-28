49a93d3cabe4d82127d05219d7f5c020d32350ee
55Nights <1905587@students.kca.ac.ke>
2022-05-28 11:02:22 +0000

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog
+#define _dog
+/**
+ * struct dog - has the following members
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: This structure creates a prototype to dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
