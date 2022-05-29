2447145f0164d0df51bcb44789712fe90ef0d721
55Nights <1905587@students.kca.ac.ke>
2022-05-29 10:51:34 +0000

    array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
-	if (action)
+	if (action != NULL && array != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
