d7226bb1e5888119608a5c387f576c4dfcb280eb
55Nights <1905587@students.kca.ac.ke>
2022-06-11 18:55:24 +0000

    2-print

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "2-print_strings.c"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    print_strings(", ", 2, "Jay", "Django");
+	        return (0);
+}
