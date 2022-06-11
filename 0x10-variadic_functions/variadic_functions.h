8d4ba1497fc6272ae6b28ab2e25ce661eccfd0ac
55Nights <1905587@students.kca.ac.ke>
2022-06-11 17:53:50 +0000

    print_numbers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
+#ifndef _VARIADIC_FUNCTION
+#define _VARIADIC_FUNCTION
 #include <stdarg.h>
 #include <stdio.h>
 #include <stddef.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
+
+#endif
