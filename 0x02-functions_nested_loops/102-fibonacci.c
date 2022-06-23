82be8b3f036a5cf93048c45d2b0dbaded3c3c7b5
55Nights <1905587@students.kca.ac.ke>
2022-06-23 09:22:30 +0000

    102-fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints 50 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i = 0, x = 0, y = 1, sum = 0;
+
+	while (i <= 50)
+	{
+		sum = x + y;
+		x = y;
+		y = sum;
+		if (i != 50)
+		{
+			printf("%li, ", sum);
+		}
+		else
+		{
+			printf("%li", sum);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
