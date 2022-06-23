37efe6f73bd187894031db2d72bfc259c36fc5c2
55Nights <1905587@students.kca.ac.ke>
2022-06-23 09:25:12 +0000

    102-fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	unsigned long int i = 0, x = 0, y = 1, sum = 0;
 
-	while (i <= 50)
+	while (i < 50)
 	{
 		sum = x + y;
 		x = y;
 		y = sum;
-		if (i != 50)
+		if (i != 49)
 		{
 			printf("%li, ", sum);
 		}
