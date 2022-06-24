304d187771914e987c8ff1093428f5004fae109e
55Nights <1905587@students.kca.ac.ke>
2022-06-24 09:41:54 +0000

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets a string
+ *
+ * @s: pointer to pointer of char
+ * @to: pointer to char
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
