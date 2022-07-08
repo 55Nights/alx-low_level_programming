1afebe0536cae3eac463bf186859a910000fd629
55Nights <1905587@students.kca.ac.ke>
2022-07-08 22:12:27 +0300

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+
+#include "lists.h"
+
+/**
+ * add_node - adds a new node
+ *
+ * @head: double pointer
+ *
+ * @str: string
+ * Return: address
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_l;
+	char *s = strdup(str);
+
+	if (head != NULL)
+	{
+		new_l = (list_t *) malloc(sizeof(list_t));
+		new_l->next = *(head);
+		new_l->str = s;
+		*(head) = new_l;
+		return (new_l);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
