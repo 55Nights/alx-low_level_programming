021ddf844aa7991908881a65b2df34dc74cbd91d
55Nights <1905587@students.kca.ac.ke>
2022-07-08 22:19:30 +0300

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,6 +17,9 @@ list_t *add_node(list_t **head, const char *str)
 	if (head != NULL)
 	{
 		new_l = (list_t *) malloc(sizeof(list_t));
+		if (new_l == NULL || s == NULL)
+		{
+			free(new);
 		new_l->next = *(head);
 		new_l->str = s;
 		*(head) = new_l;
