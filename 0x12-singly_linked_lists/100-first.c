7edca58a918e9d7a8fa5492fa5f2357760fb707e
55Nights <1905587@students.kca.ac.ke>
2022-07-22 13:24:12 +0300

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 void print_it(void) __attribute__ ((constructor));
 void print_it(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("bore my house upon my back!\n");
 }
